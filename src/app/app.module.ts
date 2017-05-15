7c63cfa9769029b1b348a35b8cb32aedec0a377c
Stephen Lander <stephenclander@gmail.com>
2017-05-15 14:50:30 -0400

    some game stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SocketService } from './socket.service';
+import { GameComponent } from './game/game.component';
+import { BoardComponent } from './game/board/board.component';
+import { TokenComponent } from './game/new-board/token/token.component';
+import { NewBoardComponent } from './game/new-board/new-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    BoardComponent,
+    TokenComponent,
+    NewBoardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
